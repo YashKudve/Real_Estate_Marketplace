a2dcd41388e6f63155fdb2b70c8e3ac8dcfb7abf
Yash <kudveyash@gmail.com>
2024-01-13 17:57:57 +0530

    Major Updates

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About';
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Header from './Components/Header';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignIn() {
+    return (
+        <div>
+            Sigin
+        </div>
+    )
+}
+
+export default SignIn
