266eb5612b41f1a89d518a025b7b32a94b04e238
Yash <kudveyash@gmail.com>
2024-01-15 12:10:45 +0530

    signup api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,9 +1,20 @@
 import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
 
 export const signup = async (req, res) => {
     const { username, email, password } = req.body;
-    const newUser = new User({ username, email, password });//to save the incoming data in the previously created model - "User"
-    await newUser.save() //to save the data in database
-    //saving user may take time depending upon speed of internet... await function stops the execution at this line until the step is completed successfully.
-    res.status(201).json('User Created Succesfully')
+
+    const hashedPassword = bcryptjs.hashSync(password, 10) // 10 is number of rounds for hasing
+
+    const newUser = new User({ username, email, password: hashedPassword });//to save the incoming data in the previously created model - "User"
+
+    try {
+        await newUser.save() //to save the data in database
+        //saving user may take time depending upon speed of internet... await function stops the execution at this line until the step is completed successfully.
+        res.status(201).json('User Created Succesfully')
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+
+
 }
