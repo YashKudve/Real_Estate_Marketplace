d439f7896b762c0da8ce6bc315b397dedc99f230
Yash <kudveyash@gmail.com>
2024-01-16 12:31:16 +0530

    sign in api routed created

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -16,7 +17,7 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error)
 
-        //next(errorHandler(550, 'error from the function')) MAnual error message
+        //next(errorHandler(550, 'error from the function')) Manual error message
     }
 }
 
@@ -29,7 +30,12 @@ export const signin = async (req, res, next) => {
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(401, 'Incorrect Credentials'));
 
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+        const { password: pass, ...rest } = validUser._doc; //To hide password from being retrieved even if it is hashed.
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+
     } catch (error) {
-        next(error) //indes.js file contains middleware
+        next(error) //index.js file contains middleware; this methods maps to middleware
     }
 }
