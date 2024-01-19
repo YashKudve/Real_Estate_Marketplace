c7c5eeeef6c78a8fa8101d523c83020ed55ebcd5
Yash <kudveyash@gmail.com>
2024-01-19 19:15:19 +0530

    user authentication

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,23 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error";
+import bcryptjs from 'bcryptjs'
+
 export const test = (req, res) => {
     res.json({
         message: "Hello Folks"
     })
+}
+
+export const updateUser = (req, res, next) => {
+    if (req, user, id !== req.params.id) return next(errorHandler(401, "Unauthorized!"))
+
+    try {
+        if (req, body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
+        }
+
+        const updatedUser = new User.findByIdAndUpdate(req.params.id,)
+    } catch (error) {
+        next(error)
+    }
 }
